f8e4b07ab82484c0ba4f3942129d05f9e83f4c9f
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-03-27 06:48:27 +0300

    Skip enrolment check in staging

--- src/services/enrolment.ts
+++ src/services/enrolment.ts
@@ -1,6 +1,9 @@
+import { inStaging } from '../util/config'
 import importerClient from '../util/importerClient'
 
 const checkEnrolment = async (studentId: string, enrolmentId: string) => {
+  if (inStaging) return true
+
   const { data: enrolments } = await importerClient.get(
     `/course_unit_realisations/${enrolmentId}/enrolments`
   )
