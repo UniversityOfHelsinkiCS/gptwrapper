1be6b991443e750bd760aee80960477ab501f50e
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-04-18 13:44:18 +0300

    React query

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import CssBaseline from '@mui/material/CssBaseline'
 
 import initializeI18n from './util/i18n'
+import queryClient from './util/queryClient'
 import App from './App'
 
 initializeI18n()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <CssBaseline>
-      <App />
-    </CssBaseline>
+    <QueryClientProvider client={queryClient}>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+      <ReactQueryDevtools position="bottom-right" />
+    </QueryClientProvider>
 
   </React.StrictMode>,
 )
