4d28713f6b8648321b5caf8ae4cbc1dd13687bb7
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-04-20 15:24:56 +0300

    Better response component

--- src/client/components/Chat/Conversation.tsx
+++ src/client/components/Chat/Conversation.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Paper } from '@mui/material'
+import { Person, Storage } from '@mui/icons-material'
 
-import { Message } from '../../types'
+import { Message, Role } from '../../types'
+
+const Response = ({ role, content }: { role: Role, content: string }) => {
+  const isUser = role === 'user'
+
+  return (
+    <Box mb={2}>
+      <Box display="flex">
+        <Box display="inline-block">
+          <Paper variant="outlined">
+            <Box p={1} display="flex">
+              {isUser ? <Person sx={{ mr: 1 }} /> : <Storage sx={{ mr: 1 }} />}
+              <Typography variant="body1">{content}</Typography>
+            </Box>
+          </Paper>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
 
 const Conversation = ({ messages }: { messages: Message[] }) => (
   <Box>
     {messages.map(({ role, content }) => (
-      <Box mb={2} key={content}>
-        <Typography variant="body1">{role}: {content}</Typography>
-      </Box>
+      <Response key={content} role={role} content={content} />
     ))}
   </Box>
 )
