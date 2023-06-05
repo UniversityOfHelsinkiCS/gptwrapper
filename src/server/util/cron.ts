f6021404addbdc22910e5f9daeb414b30fc6c8c6
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-06-05 12:16:50 +0300

    Reset global campus usage once a month

--- src/server/util/cron.ts
+++ src/server/util/cron.ts
@@ -1,16 +1,46 @@
 import cron from 'node-cron'
+import { Op, QueryTypes } from 'sequelize'
 
 import logger from './logger'
 import { sequelize } from '../db/connection'
+import { User } from '../db/models'
 
 const resetUsage = async () => {
   logger.info('Resetting usage')
-  await sequelize.query('UPDATE user_service_usages SET usage_count = 0')
+
+  await sequelize.query('UPDATE user_service_usages SET usage_count = 0', {
+    type: QueryTypes.UPDATE,
+  })
+}
+
+const resetGlobalCampusUsage = async () => {
+  logger.info('Resetting global campus usage')
+
+  const glocalCampusUsers = await User.findAll({
+    where: {
+      iamGroups: {
+        [Op.contains]: ['grp-curregc'],
+      },
+    },
+    attributes: ['id'],
+  })
+
+  const userIds = glocalCampusUsers.map((user) => user.id)
+
+  await sequelize.query(
+    'UPDATE user_service_usages SET usage_count = 0 WHERE user_id IN (:userIds)',
+    {
+      replacements: { userIds },
+      type: QueryTypes.UPDATE,
+    }
+  )
 }
 
 const setupCron = async () => {
   logger.info('Starting cron jobs')
+
   cron.schedule('0 0 1 */3 *', resetUsage) // Every three months
+  cron.schedule('0 0 1 * *', resetGlobalCampusUsage) // Once a month
 }
 
 export default setupCron
