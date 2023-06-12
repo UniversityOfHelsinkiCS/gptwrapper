7a5cca7fdbc3f0fbabf79f528143f785b80237e9
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-06-12 11:08:37 +0300

    Fix wrong iam group

--- src/server/util/config.ts
+++ src/server/util/config.ts
@@ -24,7 +24,7 @@ const curregptIam = 'grp-curregpt'
 
 export const doubleUsageIams = [globalCampusIam, curregptIam]
 
-export const tikeIam = 'grp-tike-allstaff'
+export const tikeIam = 'hy-tike-allstaff'
 
 export const accessIams = [
   globalCampusIam,
