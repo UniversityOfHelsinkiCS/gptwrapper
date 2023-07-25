db505de4d2d00106e02583911d83703cf8cd35dd
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-07-25 14:12:47 +0300

    Client side validation as well

--- src/client/components/Admin/index.tsx
+++ src/client/components/Admin/index.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 import { Box, Button, Modal } from '@mui/material'
 import { useTranslation } from 'react-i18next'
+import { Navigate } from 'react-router-dom'
 
+import useCurrentUser from '../../hooks/useCurrentUser'
 import AccessGroupTable from './AccessGroupTable'
 import CreateAccessGroup from './CreateAccessGroup'
 
 const Admin = () => {
+  const { user, isLoading } = useCurrentUser()
+
   const [createFormOpen, setCreateFormOpen] = useState(false)
   const [editFormOpen, setEditFormOpen] = useState(false)
 
   const { t } = useTranslation()
+
+  if (isLoading) return null
+  if (!user?.isAdmin) return <Navigate to="/" />
 
   return (
     <Box sx={{ margin: '0 auto', width: '90%', padding: '5%' }}>
