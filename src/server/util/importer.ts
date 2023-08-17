e3c4b1872ca6a49b3558296c6d286e314cd404a4
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-08-17 13:19:37 +0300

    Filter inactive enrollments

--- src/server/util/importer.ts
+++ src/server/util/importer.ts
@@ -1,13 +1,33 @@
 import { IMPORTER_URL, API_TOKEN } from './config'
 import { Enrollment } from '../types'
 
+const getActiveEnrollments = (enrollments: Enrollment[]) => {
+  const filteredEnrollments = enrollments.filter((enrollment) => {
+    if (enrollment.state !== 'ENROLLED') return false
+    if (!enrollment.courseUnitRealisation.id) return false
+
+    const { startDate, endDate } =
+      enrollment.courseUnitRealisation.activityPeriod
+
+    const now = new Date()
+    const start = new Date(startDate)
+    const end = new Date(endDate)
+
+    return now >= start && now <= end
+  })
+
+  return filteredEnrollments
+}
+
 const getEnrollments = async (userId: string): Promise<Enrollment[]> => {
   const url = `${IMPORTER_URL}/kliksutin/enrollments/${userId}`
 
   const response = await fetch(`${url}?token=${API_TOKEN}`)
   const data = await response.json()
 
-  return data || []
+  const enrollments = getActiveEnrollments(data || [])
+
+  return enrollments
 }
 
 export default getEnrollments
