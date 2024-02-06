81318392758964db51722e1eae847e8f6e98bfa4
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2024-02-06 09:13:45 +0200

    Simplify reset usage cron

--- src/server/util/cron.ts
+++ src/server/util/cron.ts
@@ -2,50 +2,25 @@ import cron from 'node-cron'
 import { Op } from 'sequelize'
 
 import logger from './logger'
-import { User, UserServiceUsage, ServiceAccessGroup } from '../db/models'
+import { User } from '../db/models'
 
-type ResetCron = {
-  iamGroup: string
-  resetCron: string
+const resetUsage = async () => {
+  await User.update(
+    { usage: 0 },
+    {
+      where: {
+        usage: {
+          [Op.gt]: 0,
+        },
+      },
+    }
+  )
 }
 
 const setupCron = async () => {
   logger.info('Starting cron jobs')
 
-  const accessGroups = (await ServiceAccessGroup.findAll({
-    where: {
-      resetCron: {
-        [Op.not]: null,
-      },
-    },
-    attributes: ['iamGroup', 'resetCron'],
-  })) as ResetCron[]
-
-  accessGroups.forEach(({ iamGroup, resetCron }) => {
-    cron.schedule(resetCron, async () => {
-      logger.info(`Resetting usage for ${iamGroup}`)
-
-      const users = await User.findAll({
-        where: {
-          iamGroups: {
-            [Op.contains]: [iamGroup],
-          },
-        },
-        attributes: ['id'],
-      })
-
-      const userIds = users.map(({ id }) => id)
-
-      await UserServiceUsage.destroy({
-        where: {
-          serviceId: 'chat',
-          userId: {
-            [Op.in]: userIds,
-          },
-        },
-      })
-    })
-  })
+  cron.schedule('0 0 1 * *', resetUsage) // Reset usage every month
 }
 
 export default setupCron
