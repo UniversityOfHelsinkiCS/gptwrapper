31a27f5347a08ac0d2cddd4ef0f19738d3e2c430
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-03-22 16:25:18 +0200

    Add migration to rename service to chat instance

---
+++ src/server/db/migrations/20240322_00_rename_service_to_chat_instance.ts
@@ -0,0 +1,21 @@
+import { Migration } from '../connection'
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.renameColumn(
+    'user_service_usages',
+    'service_id',
+    'chat_instance_id'
+  )
+  await queryInterface.renameTable('services', 'chat_instances')
+  await queryInterface.renameColumn('prompts', 'service_id', 'chat_instance_id')
+}
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.renameColumn('prompts', 'chat_instance_id', 'service_id')
+  await queryInterface.renameTable('chat_instances', 'services')
+  await queryInterface.renameColumn(
+    'user_service_usages',
+    'chat_instance_id',
+    'service_id'
+  )
+}
