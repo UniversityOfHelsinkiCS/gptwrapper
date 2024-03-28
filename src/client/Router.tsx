7f7d3bb6989df315af761cdf32bd2181c2fa049a
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-03-28 10:04:35 +0200

    Use the same Chat component at / and /:courseId

--- src/client/Router.tsx
+++ src/client/Router.tsx
@@ -10,7 +10,6 @@ import { PUBLIC_URL } from '../config'
 import App from './App'
 import Admin from './components/Admin'
 import Chat from './components/Chat'
-import CourseChat from './components/Course/Chat'
 import Courses from './components/Courses'
 import EditCourse from './components/Courses/EditCourse'
 import NoAccess from './components/NoAccess'
@@ -19,7 +18,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Chat />} />
-      <Route path="/:courseId" element={<CourseChat />} />
+      <Route path="/:courseId" element={<Chat />} />
       <Route path="/courses" element={<Courses />} />
       <Route path="/courses/:id" element={<EditCourse />} />
       <Route path="/admin" element={<Admin />} />
