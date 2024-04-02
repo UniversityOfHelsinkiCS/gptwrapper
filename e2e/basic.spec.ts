ca5a233e5a91f189b05b55ffb8310be24515ac49
Veikkosuhonen <vesuvesuh@gmail.com>
2024-04-02 15:57:58 +0300

    Acual pw test now?

--- e2e/basic.spec.ts
+++ e2e/basic.spec.ts
@@ -5,9 +5,8 @@ test.beforeEach(async ({ page }) => {
 })
 
 test('Can visit home', async ({ page }) => {
-  // Assert that the page title is CurreChat
-  await page.title().then((title) => {
-    expect(title).toBe('CurreChat')
-  })
+  // Assert that the page has text "Contact support:"
+  await page.getByText('Contact support:').isVisible()
+
   await page.screenshot({ path: 'screenshots/home.png' })
 })
