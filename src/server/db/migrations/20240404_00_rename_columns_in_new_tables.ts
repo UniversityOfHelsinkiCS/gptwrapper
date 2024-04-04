177465a7c6517c288dc46179abd5be60d00134af
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-04-04 14:35:38 +0300

    Add migrations to rename columns and add missing FK constraints

---
+++ src/server/db/migrations/20240404_00_rename_columns_in_new_tables.ts
@@ -0,0 +1,31 @@
+import { Migration } from '../connection'
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.renameColumn(
+    'enrolments',
+    'chatInstanceId',
+    'chat_instance_id'
+  )
+  await queryInterface.renameColumn('enrolments', 'userId', 'user_id')
+  await queryInterface.renameColumn(
+    'responsibilities',
+    'chatInstanceId',
+    'chat_instance_id'
+  )
+  await queryInterface.renameColumn('responsibilities', 'userId', 'user_id')
+}
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.renameColumn(
+    'enrolments',
+    'chat_instance_id',
+    'chatInstanceId'
+  )
+  await queryInterface.renameColumn('enrolments', 'user_id', 'userId')
+  await queryInterface.renameColumn(
+    'responsibilities',
+    'chat_instance_id',
+    'chatInstanceId'
+  )
+  await queryInterface.renameColumn('responsibilities', 'user_id', 'userId')
+}
---
+++ src/server/db/migrations/20240404_01_add_foregn_key_constraints_to_new_tables.ts
@@ -0,0 +1,64 @@
+import { Migration } from '../connection'
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  queryInterface.addConstraint('enrolments', {
+    type: 'foreign key',
+    fields: ['user_id'],
+    name: 'FK_enrolments_user_id',
+    references: {
+      table: 'users',
+      field: 'id',
+    },
+    onDelete: 'cascade',
+    onUpdate: 'cascade',
+  })
+  queryInterface.addConstraint('enrolments', {
+    type: 'foreign key',
+    fields: ['chat_instance_id'],
+    name: 'FK_enrolments_chat_instance_id',
+    references: {
+      table: 'chat_instances',
+      field: 'id',
+    },
+    onDelete: 'cascade',
+    onUpdate: 'cascade',
+  })
+  queryInterface.addConstraint('responsibilities', {
+    type: 'foreign key',
+    fields: ['user_id'],
+    name: 'FK_responsibilities_user_id',
+    references: {
+      table: 'users',
+      field: 'id',
+    },
+    onDelete: 'cascade',
+    onUpdate: 'cascade',
+  })
+  queryInterface.addConstraint('responsibilities', {
+    type: 'foreign key',
+    fields: ['chat_instance_id'],
+    name: 'FK_responsibilities_chat_instance_id',
+    references: {
+      table: 'chat_instances',
+      field: 'id',
+    },
+    onDelete: 'cascade',
+    onUpdate: 'cascade',
+  })
+}
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.removeConstraint('enrolments', 'FK_enrolments_user_id')
+  await queryInterface.removeConstraint(
+    'enrolments',
+    'FK_enrolments_chat_instance_id'
+  )
+  await queryInterface.removeConstraint(
+    'responsibilities',
+    'FK_responsibilities_user_id'
+  )
+  await queryInterface.removeConstraint(
+    'responsibilities',
+    'FK_responsibilities_chat_instance_id'
+  )
+}
