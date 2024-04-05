20e714d78900ceae88b37b0313fb7eed0c730d90
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-04-05 09:29:30 +0300

    Trying to fix the broken migration

--- src/server/db/migrations/20240404_00_add_uniq_index_to_chat_instance_course_id.ts
+++ src/server/db/migrations/20240404_00_add_uniq_index_to_chat_instance_course_id.ts
@@ -1,9 +1,11 @@
 import { Migration } from '../connection'
 
 export const up: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.addIndex('chat_instances', ['course_id'], {
+  await queryInterface.addIndex('chat_instances', {
     unique: true,
     name: 'chat_instances_course_id_unique',
+    fields: ['course_id'],
+    concurrently: true,
   })
 }
 
