bbfa9be7a3f657f931de78b0b9d16ffa21113f2a
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-06-03 13:30:37 +0300

    Fix TSC issues related to Sentry version update

--- src/server/util/sentry.ts
+++ src/server/util/sentry.ts
@@ -1,18 +1,14 @@
 import * as Sentry from '@sentry/node'
-import { Express } from 'express-serve-static-core'
 
 import { inDevelopment, inStaging, GIT_SHA } from '../../config'
 
-const initializeSentry = (router: Express) => {
+const initializeSentry = () => {
   if (inDevelopment || inStaging) return
 
   Sentry.init({
     dsn: 'https://9dde7af6dc3bc7deaf1e53c7def25c28@toska.cs.helsinki.fi/21',
     release: GIT_SHA,
-    integrations: [
-      new Sentry.Integrations.Http({ tracing: true }),
-      new Sentry.Integrations.Express({ router }),
-    ],
+    integrations: [Sentry.httpIntegration(), Sentry.expressIntegration()],
     tracesSampleRate: 1.0,
   })
 }
