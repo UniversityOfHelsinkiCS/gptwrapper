102fe30b2057d54fe5ffeee9112b0ddd0a503e4d
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-06-03 14:44:57 +0300

    Explicitly specify breadcrumbs=true for Sentry HTTP integration

--- src/server/util/sentry.ts
+++ src/server/util/sentry.ts
@@ -8,7 +8,10 @@ const initializeSentry = () => {
   Sentry.init({
     dsn: 'https://9dde7af6dc3bc7deaf1e53c7def25c28@toska.cs.helsinki.fi/21',
     release: GIT_SHA,
-    integrations: [Sentry.httpIntegration(), Sentry.expressIntegration()],
+    integrations: [
+      Sentry.httpIntegration({ breadcrumbs: true }),
+      Sentry.expressIntegration(),
+    ],
     tracesSampleRate: 1.0,
   })
 }
