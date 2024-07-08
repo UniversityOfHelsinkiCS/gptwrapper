46f30fea172b025dfe80597708d2b7290037ced4
erikao1998 <ersku1998@gmail.com>
2024-07-08 13:39:14 +0300

    Add tabs to the course list in the teacher view

---
+++ src/client/components/Courses/CourseList.tsx
@@ -0,0 +1,176 @@
+import React, { Fragment, useState } from 'react'
+import { addMonths } from 'date-fns'
+
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Link,
+  Paper,
+  Typography,
+} from '@mui/material'
+import { useTranslation } from 'react-i18next'
+import { Link as RouterLink } from 'react-router-dom'
+import { CoursesViewCourse } from '../../hooks/useUserCourses'
+import { useDisableCourse } from './useDisableCourse'
+import { useEnableCourse } from './useEnableCourse'
+import { formatDate } from './util'
+import { Course as CourseType } from '../../types'
+import { DEFAULT_MODEL_ON_ENABLE, DEFAULT_TOKEN_LIMIT } from '../../../config'
+
+const styles = {
+  title: {
+    fontSize: '1.1rem',
+    fontWeight: (theme) => theme.typography.fontWeightMedium,
+  },
+  details: {
+    flexDirection: 'column',
+    padding: 0,
+  },
+  icon: {
+    marginRight: (theme) => theme.spacing(1),
+  },
+}
+
+const Course = ({
+  course,
+  onEnable,
+}: {
+  course: CoursesViewCourse
+  onEnable: (course: CoursesViewCourse) => void
+}) => {
+  const { t } = useTranslation()
+  const disableMutation = useDisableCourse()
+
+  if (!course) return null
+
+  const { name, courseId, activityPeriod, isActive, isExpired } = course
+
+  return (
+    <Box mb="1rem">
+      <Paper
+        variant="outlined"
+        sx={{ p: 2, display: 'flex', alignItems: 'stretch' }}
+      >
+        <Box mr="auto">
+          <Typography variant="body1">{formatDate(activityPeriod)}</Typography>
+
+          <Link to={`/courses/${courseId}`} component={RouterLink}>
+            <Typography variant="h6">{name}</Typography>
+          </Link>
+
+          <Typography variant="body2">
+            <code>{courseId}</code>
+          </Typography>
+        </Box>
+
+        <Box display="flex" flexDirection="column" alignItems="end">
+          {isActive && (
+            <>
+              <Typography>{t('course:curreEnabled')}</Typography>
+              <Button
+                variant="contained"
+                sx={{ mt: 'auto' }}
+                onClick={() => disableMutation.mutate({ id: course.id })}
+              >
+                {t('course:disableCurre')}
+              </Button>
+            </>
+          )}
+          {isExpired && <Typography>{t('course:curreExpired')}</Typography>}
+          {!isActive && !isExpired && (
+            <>
+              <Typography>{t('course:curreNotEnabled')}</Typography>
+              <Button
+                variant="contained"
+                sx={{ mt: 'auto' }}
+                onClick={() => onEnable(course)}
+              >
+                {t('course:enableCurre')}
+              </Button>
+            </>
+          )}
+        </Box>
+      </Paper>
+    </Box>
+  )
+}
+
+const CourseList = ({ courseUnits }: { courseUnits: CoursesViewCourse[] }) => {
+  const { t } = useTranslation()
+
+  const enableMutation = useEnableCourse()
+
+  const [courseToEnable, setCourseToEnable] = useState<CourseType>(null)
+
+  const defaultActivityPeriod = courseToEnable
+    ? {
+        startDate: courseToEnable.activityPeriod.startDate,
+        endDate: addMonths(
+          courseToEnable.activityPeriod.endDate,
+          1
+        ).toDateString(),
+      }
+    : null
+
+  const activityPeriodString = courseToEnable
+    ? formatDate(defaultActivityPeriod)
+    : null
+
+  return (
+    <>
+      <Box sx={styles.details}>
+        {courseUnits.length === 0 && (
+          <Box p={2}>
+            <Typography color="textSecondary" align="center">
+              {t('teacherView:noCourses')}
+            </Typography>
+          </Box>
+        )}
+        {courseUnits.map((course) => (
+          <Course
+            key={course.id}
+            course={course}
+            onEnable={setCourseToEnable}
+          />
+        ))}
+      </Box>
+      <Dialog
+        open={courseToEnable !== null}
+        onClose={() => setCourseToEnable(null)}
+      >
+        <DialogTitle>
+          {t('course:enableCurreModalTitle', { name: courseToEnable?.name })}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {t('course:enableCurreModalText', {
+              defaultModel: DEFAULT_MODEL_ON_ENABLE,
+              defaultTokenLimit: DEFAULT_TOKEN_LIMIT,
+              activityPeriod: activityPeriodString,
+            })}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setCourseToEnable(null)}>{t('cancel')}</Button>
+          <Button
+            onClick={() => {
+              enableMutation.mutate({ id: courseToEnable.id })
+              setCourseToEnable(null)
+            }}
+            autoFocus
+            variant="contained"
+          >
+            {t('enable')}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  )
+}
+
+export default CourseList
--- src/client/components/Courses/index.tsx
+++ src/client/components/Courses/index.tsx
@@ -1,87 +1,23 @@
-import React, { useState } from 'react'
-import { addMonths } from 'date-fns'
-import { Link as RouterLink } from 'react-router-dom'
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Link,
-  Paper,
-  Typography,
-} from '@mui/material'
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react'
+import { Box, Tab, Tabs, Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
+import useUserCourses from '../../hooks/useUserCourses'
+import CourseList from './CourseList'
 
-import { Course as CourseType } from '../../types'
-import useUserCourses, { CoursesViewCourse } from '../../hooks/useUserCourses'
-import { formatDate } from './util'
-import { useEnableCourse } from './useEnableCourse'
-import { DEFAULT_MODEL_ON_ENABLE, DEFAULT_TOKEN_LIMIT } from '../../../config'
-import { useDisableCourse } from './useDisableCourse'
+interface TabPanelProps {
+  children?: React.ReactNode
+  index: number
+  value: number
+}
 
-const Course = ({
-  course,
-  onEnable,
-}: {
-  course: CoursesViewCourse
-  onEnable: (course: CoursesViewCourse) => void
-}) => {
-  const { t } = useTranslation()
-  const disableMutation = useDisableCourse()
-
-  if (!course) return null
-
-  const { name, courseId, activityPeriod, isActive, isExpired } = course
+const CustomTabPanel = (props: TabPanelProps) => {
+  const { children, value, index } = props
 
   return (
-    <Box mb="1rem">
-      <Paper
-        variant="outlined"
-        sx={{ p: 2, display: 'flex', alignItems: 'stretch' }}
-      >
-        <Box mr="auto">
-          <Typography variant="body1">{formatDate(activityPeriod)}</Typography>
-
-          <Link to={`/courses/${courseId}`} component={RouterLink}>
-            <Typography variant="h6">{name}</Typography>
-          </Link>
-          <Typography variant="body2">
-            <code>{courseId}</code>
-          </Typography>
-        </Box>
-
-        <Box display="flex" flexDirection="column" alignItems="end">
-          {isActive && (
-            <>
-              <Typography>{t('course:curreEnabled')}</Typography>
-              <Button
-                variant="contained"
-                sx={{ mt: 'auto' }}
-                onClick={() => disableMutation.mutate({ id: course.id })}
-              >
-                {t('course:disableCurre')}
-              </Button>
-            </>
-          )}
-          {isExpired && <Typography>{t('course:curreExpired')}</Typography>}
-          {!isActive && !isExpired && (
-            <>
-              <Typography>{t('course:curreNotEnabled')}</Typography>
-              <Button
-                variant="contained"
-                sx={{ mt: 'auto' }}
-                onClick={() => onEnable(course)}
-              >
-                {t('course:enableCurre')}
-              </Button>
-            </>
-          )}
-        </Box>
-      </Paper>
-    </Box>
+    <div role="tabpanel" hidden={value !== index}>
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
   )
 }
 
@@ -89,72 +25,38 @@ const Courses = () => {
   const { t } = useTranslation()
   const { courses } = useUserCourses()
 
-  const enableMutation = useEnableCourse()
+  const [value, setValue] = React.useState(0)
 
-  const [courseToEnable, setCourseToEnable] = useState<CourseType>(null)
-
-  const defaultActivityPeriod = courseToEnable
-    ? {
-        startDate: courseToEnable.activityPeriod.startDate,
-        endDate: addMonths(
-          courseToEnable.activityPeriod.endDate,
-          1
-        ).toDateString(),
-      }
-    : null
-
-  const activityPeriodString = courseToEnable
-    ? formatDate(defaultActivityPeriod)
-    : null
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
 
   return (
-    <Box>
+    <>
       <Box display="flex" gap={2}>
         <Typography variant="h5" display="inline" mb={1}>
           {t('common:courses')}
         </Typography>
       </Box>
-      <Box>
-        {courses?.map((course) => (
-          <Course
-            key={course.id}
-            course={course}
-            onEnable={setCourseToEnable}
-          />
-        ))}
-      </Box>
-
-      <Dialog
-        open={courseToEnable !== null}
-        onClose={() => setCourseToEnable(null)}
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="basic tabs example"
       >
-        <DialogTitle>
-          {t('course:enableCurreModalTitle', { name: courseToEnable?.name })}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            {t('course:enableCurreModalText', {
-              defaultModel: DEFAULT_MODEL_ON_ENABLE,
-              defaultTokenLimit: DEFAULT_TOKEN_LIMIT,
-              activityPeriod: activityPeriodString,
-            })}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setCourseToEnable(null)}>{t('cancel')}</Button>
-          <Button
-            onClick={() => {
-              enableMutation.mutate({ id: courseToEnable.id })
-              setCourseToEnable(null)
-            }}
-            autoFocus
-            variant="contained"
-          >
-            {t('enable')}
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </Box>
+        <Tab label="Kaikki" />
+        <Tab label="CurreChat" />
+        <Tab label="Päättyneet" />
+      </Tabs>
+      <CustomTabPanel value={value} index={0}>
+        <CourseList courseUnits={courses} />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <CourseList courseUnits={courses} />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <CourseList courseUnits={courses} />
+      </CustomTabPanel>
+    </>
   )
 }
 
