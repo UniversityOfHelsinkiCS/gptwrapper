efaefe355814e959e505d9a0c1f6eee0e7e06e99
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-07-11 15:20:09 +0300

    Fix TS errors

--- src/server/db/migrations/20240711_00_remove_responsibility_and_enrolments_duplicates.ts
+++ src/server/db/migrations/20240711_00_remove_responsibility_and_enrolments_duplicates.ts
@@ -4,10 +4,12 @@ export const up: Migration = async ({ context: queryInterface }) => {
   const [, metadata1] = await queryInterface.sequelize.query(`
     delete from responsibilities a USING responsibilities b WHERE a.user_id = b.user_id AND a.chat_instance_id = b.chat_instance_id and a.id < b.id;
   `)
+  // @ts-expect-error
   console.log('Deleted responsibilities duplicates', metadata1.rowCount)
   const [, metadata2] = await queryInterface.sequelize.query(`
     delete from enrolments a USING enrolments b WHERE a.user_id = b.user_id AND a.chat_instance_id = b.chat_instance_id and a.id < b.id;
   `)
+  // @ts-expect-error
   console.log('Deleted enrolments duplicates', metadata2.rowCount)
 }
 
