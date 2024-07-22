3e1354da0c1451beac2aa074250d886e29112cdc
erikao1998 <ersku1998@gmail.com>
2024-07-22 13:08:29 +0300

    Create InfoText model to db to make it possible for admins to change different texts in the app

---
+++ src/server/db/migrations/20240722_00_init_infotexts.ts
@@ -0,0 +1,34 @@
+import { DataTypes } from 'sequelize'
+
+import { Migration } from '../connection'
+
+export const up: Migration = ({ context: queryInterface }) =>
+  queryInterface.createTable('info_texts', {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    text: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+      defaultValue: {},
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  })
+
+export const down: Migration = ({ context: queryInterface }) =>
+  queryInterface.dropTable('info_texts')
--- src/server/db/seeders/index.ts
+++ src/server/db/seeders/index.ts
@@ -3,6 +3,7 @@ import { sleep } from '../../util/util'
 import logger from '../../util/logger'
 import seedUsers from './user'
 import seedChatInstances from './chatInstance'
+import seedInfoTexts from './infotexts'
 
 const seed = async () => {
   await sleep(1_000)
@@ -10,6 +11,7 @@ const seed = async () => {
   try {
     if (!inProduction) await seedUsers()
     await seedChatInstances()
+    await seedInfoTexts()
     logger.info('Seeding successful')
   } catch (e) {
     logger.error('Seeding failed: ', e)
