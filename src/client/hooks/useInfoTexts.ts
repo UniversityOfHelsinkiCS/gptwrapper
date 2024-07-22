f31d8cdf93ca8841c1d599b2b58d59775e2242c7
erikao1998 <ersku1998@gmail.com>
2024-07-22 15:07:40 +0300

    Add hook for fetching info texts from server

---
+++ src/client/hooks/useInfoTexts.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+
+import apiClient from '../util/apiClient'
+
+const queryKey = ['infotexts']
+
+const useInfoTexts = () => {
+  const queryFn = async (): Promise<any> => {
+    const res = await apiClient.get(`/infotexts`)
+
+    const { data } = res
+
+    return data
+  }
+
+  const { data: infoTexts, ...rest } = useQuery({ queryKey, queryFn })
+
+  return { infoTexts, ...rest }
+}
+
+export default useInfoTexts
