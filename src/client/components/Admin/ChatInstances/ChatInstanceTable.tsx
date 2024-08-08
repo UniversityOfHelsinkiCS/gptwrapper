14dc20df9b5e20f010a3b3fd744a6459ac1f377a
erikao1998 <ersku1998@gmail.com>
2024-08-08 09:54:58 +0300

    Disable sorting course table by used tokens for now

--- src/client/components/Admin/ChatInstances/ChatInstanceTable.tsx
+++ src/client/components/Admin/ChatInstances/ChatInstanceTable.tsx
@@ -100,18 +100,24 @@ const Head = ({
             padding={headCell.disablePadding ? 'none' : 'normal'}
             sx={{ pl: 1 }}
           >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {t(`admin:${headCell.id}`)}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
-            </TableSortLabel>
+            {headCell.id !== 'tokenUsage' ? (
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {t(`admin:${headCell.id}`)}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === 'desc'
+                      ? 'sorted descending'
+                      : 'sorted ascending'}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            ) : (
+              <TableCell align="right">{t(`admin:${headCell.id}`)}</TableCell>
+            )}
           </TableCell>
         ))}
       </TableRow>
