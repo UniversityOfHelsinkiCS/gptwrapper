7c0f406a71a510c6ed04fefe23f1c23bfc24e9d8
Henri Remonen <henri@remonen.fi>
2024-10-16 09:01:58 +0300

    feat: error handling betterments -refs: #205

--- src/client/components/Chat/error.ts
+++ src/client/components/Chat/error.ts
@@ -10,7 +10,9 @@ export const handleCompletionStreamError = (err: any, file: string) => {
     enqueueSnackbar(t('error:tooLargeFile'), { variant: 'error' })
   } else if (error === 'Error parsing file' && file) {
     enqueueSnackbar(t('error:fileParsingError'), { variant: 'error' })
+  } else if (!error && typeof err === 'string') {
+    enqueueSnackbar(err, { variant: 'error' })
   } else {
-    enqueueSnackbar(error, { variant: 'error' })
+    enqueueSnackbar(t('error:unexpected'), { variant: 'error' })
   }
 }
