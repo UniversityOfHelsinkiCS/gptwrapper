5a8daec4e2332f4f3ac3663019ece39b06159a9c
Aleksandr Tereshchenko <aleksandr.tereshchenko@helsinki.fi>
2024-10-28 15:18:07 +0200

    Fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -30,8 +30,8 @@ export default [
   },
   {
     files: ['**/*.integration-test.*', '**/*.test.*'],
-    env: {
-      jest: true,
+    languageOptions: {
+      globals: globals.jest,
     },
   },
 ]
