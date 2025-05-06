ec679a98bfbd3e8a6dfdd5285adcb8348f79b032
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-06 13:57:54 +0300

    skip pulling the ollama model since it is already pulled

--- src/server/util/ollama.ts
+++ src/server/util/ollama.ts
@@ -4,7 +4,7 @@ import { OLLAMA_HOST } from '../../config'
 const MODEL_NAME = 'snowflake-arctic-embed2'
 
 const ollama = new Ollama({ host: OLLAMA_HOST })
-
+/*
 ollama
   .pull({
     model: MODEL_NAME,
@@ -16,6 +16,8 @@ ollama
     console.error('Error pulling model:', error)
   })
 console.log('Ollama client initialized, pulling model...')
+*/
+console.log('Ollama client initialized')
 
 export const getEmbedding = async (prompt: string) => {
   const response = await ollama.embed({
