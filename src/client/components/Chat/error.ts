2452009e1947ee80b5eb832fa5e6b23a4091dc98
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-15 14:36:03 +0300

    remove unexpected error message

--- src/client/components/Chat/error.ts
+++ src/client/components/Chat/error.ts
@@ -5,19 +5,16 @@ import { enqueueSnackbar } from 'notistack'
  * Handle error messages related to completion stream creation
  */
 export const handleCompletionStreamError = (err: any, file: string) => {
-  console.log('Error in completion stream: ', err)
   if (err?.name === 'AbortError' || !err) return
-
+  console.log('Error in completion stream: ', err)
   const error = err?.response?.data || err.message
 
   if (error === 'Model maximum context reached' && file) {
     enqueueSnackbar(t('error:tooLargeFile'), { variant: 'error' })
   } else if (error === 'Error parsing file' && file) {
     enqueueSnackbar(t('error:fileParsingError'), { variant: 'error' })
-  } else if (error === 'TimeoutError') {
-    enqueueSnackbar(t('error:waitingForResponse'), { variant: 'error' })
   } else {
     console.log('Unexpected error in completion stream: ', error)
-    enqueueSnackbar(t('error:unexpected'), { variant: 'error' })
+    //enqueueSnackbar(t('error:unexpected'), { variant: 'error' })
   }
 }
