75feb541aca150ef3f16e6d13e61f8141629372f
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-15 14:51:35 +0300

    add comment

--- src/client/components/Chat/error.ts
+++ src/client/components/Chat/error.ts
@@ -9,12 +9,14 @@ export const handleCompletionStreamError = (err: any, file: string) => {
   console.log('Error in completion stream: ', err)
   const error = err?.response?.data || err.message
 
+  //there is an error code 'Creating a stream took too long' which happens with slow internet connections
+  //might want to show a loading spinner when it happens
   if (error === 'Model maximum context reached' && file) {
     enqueueSnackbar(t('error:tooLargeFile'), { variant: 'error' })
   } else if (error === 'Error parsing file' && file) {
     enqueueSnackbar(t('error:fileParsingError'), { variant: 'error' })
   } else {
-    console.log('Unexpected error in completion stream: ', error)
+    //console.log('Unexpected error in completion stream: ', error)
     //enqueueSnackbar(t('error:unexpected'), { variant: 'error' })
   }
 }
