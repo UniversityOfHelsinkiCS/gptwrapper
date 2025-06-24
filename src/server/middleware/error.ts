5bcf2665adc6361b2efc56e5b5d889d29638a09b
Veikkosuhonen <vesuvesuh@gmail.com>
2025-06-24 20:37:47 +0300

    some error handling from norppa

--- src/server/middleware/error.ts
+++ src/server/middleware/error.ts
@@ -1,13 +1,20 @@
 import * as Sentry from '@sentry/node'
-
+import type { NextFunction, Request, Response } from 'express'
 import logger from '../util/logger'
+import { ApplicationError } from '../util/ApplicationError'
 
-const errorHandler = (error: any, _req: any, _res: any, next: any) => {
+const errorHandler = (error: Error, _req: Request, res: Response, next: NextFunction) => {
   logger.error(`${error.message} ${error.name} ${error.stack}`)
 
   Sentry.captureException(error)
 
-  return next(error)
+  if (res.headersSent) {
+    return next(error)
+  }
+
+  const normalizedError = error instanceof ApplicationError ? error : new ApplicationError(error.message)
+
+  return res.status(normalizedError.status).json(normalizedError)
 }
 
 export default errorHandler
---
+++ src/server/util/ApplicationError.ts
@@ -0,0 +1,41 @@
+export class ApplicationError extends Error {
+  name: string
+  status: number
+  extra: Record<string, any>
+
+  constructor(message: string, status?: number, extra?: Record<string, any>) {
+    super()
+
+    Error.captureStackTrace(this, this.constructor)
+
+    this.name = this.constructor.name
+
+    this.message = message || 'Something went wrong. Please try again.'
+
+    this.status = status || 500
+
+    this.extra = extra || {}
+  }
+
+  toJSON() {
+    return {
+      error: this.message,
+    }
+  }
+
+  static NotFound(msg = 'Not found') {
+    return new ApplicationError(msg, 404)
+  }
+
+  static Forbidden(msg = 'Forbidden') {
+    return new ApplicationError(msg, 403)
+  }
+
+  static Conflict(msg = 'Conflict') {
+    return new ApplicationError(msg, 409)
+  }
+
+  static BadRequest(msg = 'Bad request') {
+    return new ApplicationError(msg, 400)
+  }
+}
