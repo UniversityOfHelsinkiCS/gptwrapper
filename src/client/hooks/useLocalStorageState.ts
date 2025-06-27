e7dabc9033662a07dad8c4aa7e461cbdb7a8e5e0
Veikkosuhonen <vesuvesuh@gmail.com>
2025-06-27 10:58:17 +0300

    fix localStorage hook

--- src/client/hooks/useLocalStorageState.ts
+++ src/client/hooks/useLocalStorageState.ts
@@ -1,15 +1,16 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { type Dispatch, type SetStateAction, useEffect, useState } from 'react'
 
 function useLocalStorageState<T extends object>(key: string, defaultValue: T): [T, Dispatch<SetStateAction<T>>]
 function useLocalStorageState<T extends object>(key: string): [T | undefined, Dispatch<SetStateAction<T | undefined>>]
 function useLocalStorageState<T extends object>(key: string, defaultValue = undefined) {
   const [state, setState] = useState(() => {
     const storedValue = localStorage.getItem(key)
-    return storedValue !== null ? (JSON.parse(storedValue) as T) : defaultValue
+    if (!storedValue) return defaultValue
+    return JSON.parse(storedValue) as T
   })
 
   useEffect(() => {
-    if (state !== null) localStorage.setItem(key, JSON.stringify(state))
+    if (!state) localStorage.setItem(key, JSON.stringify(state))
   }, [key, state])
 
   return [state, setState]
