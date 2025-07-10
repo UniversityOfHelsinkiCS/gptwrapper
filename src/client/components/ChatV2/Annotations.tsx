251f046520e83577983d48a80c46b81cc95a4790
sutigit <panyanilben@gmail.com>
2025-07-10 15:31:05 +0300

    annotation details drawer

--- src/client/components/ChatV2/Annotations.tsx
+++ src/client/components/ChatV2/Annotations.tsx
@@ -1,19 +1,22 @@
-import { Box, Typography, Chip, IconButton } from '@mui/material'
+import { useState } from 'react'
+import { Box, Typography, Chip, IconButton, Drawer } from '@mui/material'
 import { Close } from '@mui/icons-material'
 import { FileSearchCompletedData, FileSearchResultData } from '../../../shared/types'
 import { useTranslation } from 'react-i18next'
 import { useFileSearchResults } from './api'
-import { GrayButton } from './generics/Buttons'
-import { ShortText, Subject } from '@mui/icons-material'
 
 
 const AnnotationBox = ({
     data,
-    relevanceOrder
+    relevanceOrder,
+    setIsDrawerOpen
 }: {
     data: FileSearchResultData,
     relevanceOrder: number // By default the backend returns the RAG results in most relevant results first 
+    setIsDrawerOpen: (open: boolean) => void
 }) => {
+    const [isHovering, setIsHovering] = useState<boolean>(false)
+
     const lineNum = 3;
     const multilineEllipsisTruncate = {
         // This is a trick to achieve a multu-line ellipsis truncation for max 3 rows of text. 
@@ -29,7 +32,21 @@ const AnnotationBox = ({
     }
 
     return (
-        <Box sx={{ display: 'flex', gap: 2 }}>
+        <Box
+            sx={{
+                display: 'flex',
+                gap: 2,
+                backgroundColor: isHovering ? '#efefef' : 'transparent',
+                transition: 'background-color 0.1s ease-in-out',
+                padding: '0.8rem 0.6rem',
+                borderRadius: '0.6rem',
+                cursor: 'pointer'
+            }}
+            onClick={() => setIsDrawerOpen(true)}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+
+        >
             <Box sx={{
                 color: 'black',
                 backgroundColor: 'rgba(0,0,0,0.12)',
@@ -86,6 +103,7 @@ const Queries = ({ queries }: { queries: string[] }) => {
 
 const Annotations = ({ fileSearchResult, setShowAnnotations }: { fileSearchResult: FileSearchCompletedData; setShowAnnotations: (show: boolean) => void }) => {
     const { data: results, isSuccess: isResultsSuccess } = useFileSearchResults(fileSearchResult.id)
+    const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
     const arrayResults = Array.isArray(results) ? results : []
     const { t } = useTranslation()
 
@@ -104,16 +122,41 @@ const Annotations = ({ fileSearchResult, setShowAnnotations }: { fileSearchResul
             </Box>
             <Queries queries={fileSearchResult.queries} />
             {isResultsSuccess ?
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1.5rem', minHeight: 400 }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.3rem', minHeight: 400 }}>
                     {arrayResults.map((result, i) => (
-                        <AnnotationBox key={i} data={result} relevanceOrder={i + 1} />
+                        <AnnotationBox key={i} data={result} relevanceOrder={i + 1} setIsDrawerOpen={setIsDrawerOpen} />
                     ))}
-                    {/* <Box>
-                        <GrayButton startIcon={<Subject />}>{t('chat:readMore')}</GrayButton>
-                    </Box> */}
                 </Box> :
-                <Typography>Failed to display search results</Typography>
+                <Typography>{t('chat:failedSources')}</Typography>
             }
+            <Drawer anchor={'right'} open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+                <Box sx={{ maxWidth: '60vw', padding: '8rem 3rem', display: 'flex', gap: '3rem', flexDirection: 'column' }}>
+
+                    {arrayResults.map((result, relevanceOrder) => (
+                        <Box key={relevanceOrder} sx={{ display: 'flex', gap: 2, }}>
+                            <Box sx={{
+                                color: 'black',
+                                backgroundColor: 'rgba(0,0,0,0.12)',
+                                minWidth: '1.8rem',
+                                height: '1.8rem',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                borderRadius: '100%',
+                            }}>
+                                {relevanceOrder + 1}
+                            </Box>
+                            <Box>
+                                <Box sx={{ display: 'flex', gap: 2, mb: '0.6rem', alignItems: 'center' }}>
+                                    <Typography fontWeight={600}>{result.filename}</Typography>
+                                    <Typography sx={{ opacity: 0.7 }}>{`Score: ${result.score}`}</Typography>
+                                </Box>
+                                <Typography>{result.text}</Typography>
+                            </Box>
+                        </Box>
+                    ))}
+                </Box>
+            </Drawer>
         </Box>
     )
 }
