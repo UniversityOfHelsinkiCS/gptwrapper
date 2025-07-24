86f401622122a2cf0d38cdc7909e31503787afb0
sutigit <panyanilben@gmail.com>
2025-07-24 13:23:47 +0300

    add k6 load test script

---
+++ scripts/k6_load_test.js
@@ -0,0 +1,98 @@
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+/**
+ * This script requires docker image grafana/k6
+ *
+ * run using: docker run --rm -i grafana/k6 run - <script.js
+ */
+
+const staging = 'https://toska-staging.cs.helsinki.fi/gptwrapper/api//ai/stream'
+const local = 'http://172.17.0.1:3000/api//ai/stream'
+
+const vusit = 50
+
+export const options = {
+  vus: vusit,
+  iterations: vusit,
+  // duration: "1s",
+}
+
+const messages = [
+  'listaa viisi numeroa',
+  'listaa viisi väriä',
+  'listaa viisi esinettä',
+  'listaa viisi makua',
+  'listaa viisi huonekalua',
+  'listaa viisi presidenttiä',
+  'listaa viisi maata',
+  'listaa viisi ohjelmointikieltä',
+  'listaa viisi kaupunkia',
+  'listaa viisi vaatemerkkiä',
+  'listaa viisi urheilulajia',
+]
+
+const data = {
+  options: {
+    messages: [
+      { role: 'system', content: 'Olet avulias avustaja' },
+      {
+        role: 'user',
+        content: undefined, // will be substituted from messages list
+      },
+    ],
+    assistantInstructions: 'Olet avulias avustaja',
+    model: 'gpt-4o-mini',
+    modelTemperature: 0.5,
+    saveConsent: false,
+    prevResponseId: '',
+  },
+}
+
+function tokensToText(tokenizedStr) {
+  return tokenizedStr
+    .split('\n')
+    .filter(Boolean)
+    .map((line) => {
+      try {
+        return JSON.parse(line).text
+      } catch {
+        return ''
+      }
+    })
+    .join('')
+}
+
+export default function () {
+  const url = `${local}/v2`
+
+  for (const message of messages) {
+    const dataCopy = { ...data }
+    dataCopy.options.messages = [
+      { role: 'system', content: 'Olet load testissä' },
+      {
+        role: 'user',
+        content: message,
+      },
+    ]
+    const updatedData = dataCopy
+
+    const payload = JSON.stringify({
+      data: JSON.stringify(updatedData),
+    })
+    const params = {
+      headers: {
+        'Content-Type': 'application/json',
+        uid: 'testUser',
+        mail: 'veikko@toska.test.dev',
+        preferredlanguage: 'fi',
+        hypersonsisuid: 'hy-hlo-123',
+        hygroupcn: 'grp-toska;hy-employees;grp-currechat-demostudents;grp-currechat-demoteachers',
+      },
+    }
+
+    let res = http.post(url, payload, params)
+    console.log('📌', tokensToText(res.body))
+    check(res, { 'status is 200': (res) => res.status === 200 })
+    sleep(5)
+  }
+}
