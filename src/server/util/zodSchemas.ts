5197fb9d31dc927b50d099f682aac9e7de832b65
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-08-11 12:17:32 +0300

    allow id to be any string

--- src/server/util/zodSchemas.ts
+++ src/server/util/zodSchemas.ts
@@ -1,5 +1,3 @@
 import { z } from 'zod'
 
-export const cleanIdStringSchema = z.string().regex(/^[a-zA-Z0-9]+$/, {
-  message: 'String must only contain letters',
-})
+export const cleanIdStringSchema = z.string()
