f3d0abe1c4889e1e651ab05a4de03e8cfd842e01
tfhuhtal <tuomas.huhtala@icloud.com>
2025-08-28 13:56:32 +0300

    fix: rm v1 router

--- src/server/routes/ai/index.ts
+++ src/server/routes/ai/index.ts
@@ -1,11 +1,9 @@
 import express from 'express'
 import fileSearchResultsRouter from './fileSearchResults'
-import v1Router from './v1'
 import v3Router from './v3'
 
 const aiRouter = express.Router()
 
-aiRouter.use('/v1', v1Router)
 aiRouter.use('/v3', v3Router)
 
 aiRouter.use('/toolResults', fileSearchResultsRouter)
