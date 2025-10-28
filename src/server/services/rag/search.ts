37c81bbb3728e43a0d19b9273615c642f2fa3aef
Veikkosuhonen <vesuvesuh@gmail.com>
2025-10-28 14:40:07 +0200

    Tweak rag search weights and k

--- src/server/services/rag/search.ts
+++ src/server/services/rag/search.ts
@@ -8,14 +8,14 @@ import { curateDocuments } from './curator'
 export const search = async (ragIndex: RagIndex, searchParams: SearchParams): Promise<{ results: RagChunk[]; timings: Record<string, number> }> => {
   const timings: Record<string, number> = {}
 
-  const vectorstoreRetriever = getVectorSearchRetriever(`ragIndex-${ragIndex.id}`, 13)
+  const vectorstoreRetriever = getVectorSearchRetriever(`ragIndex-${ragIndex.id}`, 8)
 
   const retrievers: BaseRetriever[] = []
   const weights: number[] = []
 
   if (searchParams.vector) {
     retrievers.push(vectorstoreRetriever)
-    weights.push(0.4)
+    weights.push(0.3)
   }
 
   if (searchParams.ft) {
@@ -28,7 +28,7 @@ export const search = async (ragIndex: RagIndex, searchParams: SearchParams): Pr
       ],
     )
     // weights.push(0.4)
-    weights.push(...[1.0, 0.4, 0.4, 0.4])
+    weights.push(...[1.0, 0.6, 0.5, 0.4])
   }
 
   const retriever = new EnsembleRetriever({
@@ -38,11 +38,10 @@ export const search = async (ragIndex: RagIndex, searchParams: SearchParams): Pr
 
   timings.search = Date.now()
   let results = await retriever.invoke(searchParams.query)
-  console.log('Search found results:', results.length)
   timings.search = Date.now() - timings.search
 
-  // Take top 9 results before curation
-  results = results.slice(0, 9)
+  // Take top 10 results before curation
+  results = results.slice(0, 10)
 
   if (searchParams.curate) {
     timings.curation = Date.now()
