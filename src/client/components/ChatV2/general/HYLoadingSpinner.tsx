a97a234c569c4479d84a5765f1dbeb5940af700c
sutigit <panyanilben@gmail.com>
2025-11-04 15:46:39 +0200

    proper loading

--- src/client/components/ChatV2/general/HYLoadingSpinner.tsx
+++ src/client/components/ChatV2/general/HYLoadingSpinner.tsx
@@ -1,22 +1,27 @@
-import { Box, CircularProgress } from '@mui/material'
+import { Box, CircularProgress, Typography } from '@mui/material'
 import hyLogo from '../../../assets/hy_logo.svg'
+import { useTranslation } from 'react-i18next'
 
 export default function HYLoadingSpinner() {
+    const { t } = useTranslation()
 
-    return (<Box sx={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-        <Box
-            component="img"
-            src={hyLogo}
-            alt="University of Helsinki"
-            sx={{
-                width: { xs: '140px', sm: '240px', md: '300px' },
-                opacity: 0.2,
-                m: '2.5rem 0',
-            }}
-        />
-        <Box sx={{ display: 'flex' }}>
-            <CircularProgress sx={{ color: "rgba(0,0,0,0.2)" }} />
+    return (
+        <Box sx={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+            <Box
+                component="img"
+                src={hyLogo}
+                alt="University of Helsinki"
+                sx={{
+                    width: { xs: '80px', sm: '120px', md: '200px' },
+                    opacity: 0.2,
+                    mb: 4
+                }}
+            />
+            <Box sx={{ display: 'flex', alignItems: 'center', opacity: 0.5 }}>
+                <Typography variant='h5' fontStyle="italic">{t("common:loading")}...</Typography>
+                <CircularProgress size="2rem" sx={{ ml: 4, color: 'black' }} />
+            </Box>
         </Box>
-    </Box>)
+    )
 
 }
