1a9fcde08858cf6199bfac54a1aa4346f27b10af
Tuomas <105309970+tfhuhtal@users.noreply.github.com>
2025-12-10 13:33:54 +0200

    Revert "Increase file upload limit from 10MB to 50MB (#517)"
    This reverts commit 8e57a8398961d7e4d119efb81616353375325454.

--- src/server/routes/ai/multer.ts
+++ src/server/routes/ai/multer.ts
@@ -1,9 +1,4 @@
 import multer from 'multer'
 
 const storage = multer.memoryStorage()
-export const upload = multer({
-  storage,
-  limits: {
-    fileSize: 50 * 1024 * 1024, // 50 MB
-  },
-})
+export const upload = multer({ storage })
