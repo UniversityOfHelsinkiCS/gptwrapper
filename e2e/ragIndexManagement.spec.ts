37c95cc24dc0b776c5ebfc088d90eaaaab14f855
Veikkosuhonen <vesuvesuh@gmail.com>
2026-01-17 18:02:21 +0200

    test: Fixsx

--- e2e/ragIndexManagement.spec.ts
+++ e2e/ragIndexManagement.spec.ts
@@ -12,16 +12,16 @@ test.describe('Rag index management', () => {
     await page.getByTestId('course-settings-button').click()
     await page.getByTestId('sourceMaterialsTab').click()
     await page.getByTestId('createNewRagButton').click()
-    await page.getByTestId('ragIndexNameInput').fill('perkelee')
+    await page.getByTestId('ragIndexNameInput').fill('perkele')
     await page.getByTestId('ragIndexLanguageInput').click()
     await page.getByTestId('ragIndexLanguageOptionFinnish').click()
     await page.getByTestId('ragIndexCreateSubmit').click()
-    await expect(page.getByText('perkelee')).toBeVisible()
+    await expect(page.getByText('perkele')).toBeVisible()
 
     // Go back
     await page.getByTestId('ragIndexBackToList').click()
 
-    await expect(page.getByText('perkelee')).toBeVisible()
+    await expect(page.getByText('perkele')).toBeVisible()
     // Should be first in list now
     await page.getByTestId('ragIndexDetails').first().click()
 
@@ -30,23 +30,23 @@ test.describe('Rag index management', () => {
 
     await expect(page.getByTestId('ragIndexDeleteSuccessSnackbar')).toBeVisible()
 
-    await expect(page.getByText('perkelee')).not.toBeVisible()
+    await expect(page.getByText('perkele')).not.toBeVisible()
   })
 
   test('Can change the name of a RAG index', async ({ page }) => {
     await page.getByTestId('course-settings-button').click()
     await page.getByTestId('sourceMaterialsTab').click()
     await page.getByTestId('createNewRagButton').click()
-    await page.getByTestId('ragIndexNameInput').fill('perkelee')
+    await page.getByTestId('ragIndexNameInput').fill('pahaminttu')
     await page.getByTestId('ragIndexLanguageInput').click()
     await page.getByTestId('ragIndexLanguageOptionFinnish').click()
     await page.getByTestId('ragIndexCreateSubmit').click()
-    await expect(page.getByText('perkelee')).toBeVisible()
+    await expect(page.getByText('pahaminttu')).toBeVisible()
 
     // Go back
     await page.getByTestId('ragIndexBackToList').click()
 
-    await expect(page.getByText('perkelee')).toBeVisible()
+    await expect(page.getByText('pahaminttu')).toBeVisible()
     // Should be first in list now
     await page.getByTestId('ragIndexDetails').first().click()
 
@@ -54,12 +54,12 @@ test.describe('Rag index management', () => {
     await page.getByTestId('ragIndexNameEditInput').fill('minttu')
     await page.getByTestId('ragIndexNameEditSave').click()
     await expect(page.getByText('minttu')).toBeVisible()
-    await expect(page.getByText('perkelee')).not.toBeVisible()
+    await expect(page.getByText('pahaminttu')).not.toBeVisible()
 
     // Go back
     await page.getByTestId('ragIndexBackToList').click()
 
     await expect(page.getByText('minttu')).toBeVisible()
-    await expect(page.getByText('perkelee')).not.toBeVisible()
+    await expect(page.getByText('pahaminttu')).not.toBeVisible()
   })
 })
