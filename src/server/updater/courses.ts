e380ae9008447f706058e518b5b60c0d3e1bce6f
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-02-03 15:01:32 +0200

    refactor: remove unused helper function

--- src/server/updater/courses.ts
+++ src/server/updater/courses.ts
@@ -8,33 +8,6 @@ import { safeBulkCreate } from './util'
 
 type Lang = keyof Locales
 
-// Find the newest course unit that has started before the course realisation
-const getCourseUnit = (courseUnits: SisuCourseUnit[], activityPeriod: ActivityPeriod) => {
-  let courseUnit = courseUnits[0] // old default
-
-  const { startDate: realisationStartDate } = activityPeriod
-
-  courseUnits.sort((a, b) => {
-    const { startDate: aStartDate } = a.validityPeriod
-    const { startDate: bStartDate } = b.validityPeriod
-
-    if (!aStartDate || !bStartDate) return 0
-
-    return Date.parse(aStartDate) - Date.parse(bStartDate)
-  })
-
-  courseUnit =
-    courseUnits.find(({ validityPeriod }) => {
-      const { startDate } = validityPeriod
-
-      if (!startDate) return false
-
-      return Date.parse(realisationStartDate) > Date.parse(startDate)
-    }) ?? courseUnit
-
-  return courseUnit
-}
-
 const courseUnitsOf = ({ courseUnits }: any) => {
   const relevantFields = courseUnits.map((unit) => ({
     code: unit.code,
