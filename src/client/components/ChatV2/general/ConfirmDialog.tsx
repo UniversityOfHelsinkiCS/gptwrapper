df20b9a86f40e85e2329089539b929506d7db4c2
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-03-24 15:00:44 +0200

    Add reusable confirm dialog

---
+++ src/client/components/ChatV2/general/ConfirmDialog.tsx
@@ -0,0 +1,37 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+
+const ConfirmDialog = ({
+  open,
+  title,
+  message,
+  onConfirm,
+  onCancel,
+}: {
+  open: boolean
+  title: string
+  message: string
+  onConfirm: () => void
+  onCancel: () => void
+}) => {
+  const { t } = useTranslation()
+
+  return (
+    <Dialog open={open} onClose={onCancel}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{message}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel} variant="text">
+          {t('common:cancel')}
+        </Button>
+        <Button onClick={onConfirm} variant="contained" color="error">
+          {t('common:delete')}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default ConfirmDialog
