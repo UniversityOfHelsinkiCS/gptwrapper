3e250e4205f90589239f1d90220259d41a8d027c
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-04-14 16:01:19 +0300

    changing a tab cancels editing

--- src/client/components/ChatV2/PromptModal.tsx
+++ src/client/components/ChatV2/PromptModal.tsx
@@ -144,6 +144,7 @@ const PromptModal = () => {
         onChange={(_, newValue) => {
           setTab(newValue)
           setCreateNewOpen(false)
+          setIsEditing(false)
         }}
         slotProps={{
           indicator: { sx: { backgroundColor: 'text.primary' } },
