de93dcc269cec6a27661cb620834c702412ee730
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-04-21 12:15:32 +0300

    add gpt 5.1 to config

--- src/server/util/tiktoken.ts
+++ src/server/util/tiktoken.ts
@@ -2,7 +2,9 @@ import { encoding_for_model, TiktokenModel } from '@dqbd/tiktoken'
 import { ValidModelName } from '../../config'
 
 const getEncoding = (model: ValidModelName) => {
-  if (model === 'mock') {
+  // 21.4.26 Tiktoken does not 'support' anything above gpt 5. So:
+  // 5.1 is aliased to 4o mini encoder, as gpt 4 and 5 use the same encoder. If this becomes a pattern, this can be refactored for all openAi models.
+  if (model === 'mock' || model === 'gpt-5.1') {
     model = 'gpt-4o-mini'
   }
 
