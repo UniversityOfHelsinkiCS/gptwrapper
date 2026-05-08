57e950f95a1df923a2a5530f0acaae251d0fab45
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-05-08 12:29:45 +0300

    remove model and temp columns from prompts table

---
+++ src/server/db/migrations/20260508_00_remove_model_and_temp_from_prompts.ts
@@ -0,0 +1,19 @@
+import { DataTypes } from 'sequelize'
+
+import type { Migration } from '../connection'
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.removeColumn('prompts', 'model')
+  await queryInterface.removeColumn('prompts', 'temperature')
+}
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.addColumn('prompts', 'model', {
+    type: DataTypes.STRING,
+    allowNull: true,
+  })
+  await queryInterface.addColumn('prompts', 'temperature', {
+    type: DataTypes.FLOAT,
+    allowNull: true,
+  })
+}
