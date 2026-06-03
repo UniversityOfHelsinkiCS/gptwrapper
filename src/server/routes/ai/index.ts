379dc32c3cd847fac888333ee68fd13047063611
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-06-03 14:12:43 +0300

    remove v4 and agent.ts

--- src/server/routes/ai/index.ts
+++ src/server/routes/ai/index.ts
@@ -1,13 +1,10 @@
 import express from 'express'
 import fileSearchResultsRouter from './fileSearchResults'
-import agentRouter from './v4'
 import v3Router from './v3'
 
 const aiRouter = express.Router()
 
 aiRouter.use('/v3', v3Router)
-
-aiRouter.use('/v4', agentRouter)
 
 aiRouter.use('/toolResults', fileSearchResultsRouter)
 
