647adcf35200836774f03a57ead19f54fa3251c0
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-08-03 09:07:52 +0300

    make 401 not throw like it was supposed to +make retry logic tighter

--- src/client/hooks/useCurrentUser.ts
+++ src/client/hooks/useCurrentUser.ts
@@ -3,12 +3,15 @@ import { useQuery } from '@tanstack/react-query'
 
 import type { User } from '../types'
 import apiClient from '../util/apiClient'
+import type { ApiError } from '../util/apiClient'
 
 const queryKey = ['login']
 
 const useCurrentUser = () => {
   const queryFn = async () => {
-    const res = await apiClient.get<User>(`/users/login`)
+    const res = await apiClient.get<User>(`/users/login`, {
+      validateStatus: (status) => status === 200 || status === 401,
+    })
 
     if (res.status === 401) return null
 
@@ -30,6 +33,11 @@ const useCurrentUser = () => {
     // staleTime ensures that the data is not refetched
     // unnecessarily within a short period of time.
     staleTime: 5000,
+    retry: (failureCount, error) => {
+      const status = (error as ApiError).response?.status
+      if (status && status >= 400 && status < 500) return false
+      return failureCount < 2
+    },
   })
 
   return { user, ...rest }
