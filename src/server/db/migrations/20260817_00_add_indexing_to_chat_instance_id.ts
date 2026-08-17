ca36b8a4872e2ab789b01e76e093ff12f72c21a9
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-08-17 13:49:31 +0300

    fix typo

--- src/server/db/migrations/20260817_00_add_indexing_to_chat_instance_id.ts
+++ src/server/db/migrations/20260817_00_add_indexing_to_chat_instance_id.ts
@@ -11,5 +11,5 @@ export const up: Migration = async ({ context: queryInterface }) => {
 
 export const down: Migration = async ({ context: queryInterface }) => {
   await queryInterface.removeIndex('prompts', 'prompts_chat_instance_id_idx')
-  await queryInterface.removeIndex('prompts', 'responsibilities_chat_instance_id_idx')
+  await queryInterface.removeIndex('responsibilities', 'responsibilities_chat_instance_id_idx')
 }
