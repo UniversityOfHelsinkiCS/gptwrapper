9c7e0c5dc7ca5ed05f59f4124982cce10750e3d4
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-08-20 09:57:11 +0300

    add unique to migration

--- src/server/db/migrations/20260819_00_add_prompts_chat_instances_indexes.ts
+++ src/server/db/migrations/20260819_00_add_prompts_chat_instances_indexes.ts
@@ -2,10 +2,11 @@ import type { Migration } from '../connection'
 
 export const up: Migration = async ({ context: queryInterface }) => {
   await queryInterface.addIndex('prompts_chat_instances', ['chat_instance_id', 'prompt_id'], {
-    name: 'prompts_chat_instances_chat_instance_id_prompt_id_idx',
+    name: 'prompts_chat_instances_chat_instance_id_prompt_id_uniq',
+    unique: true,
   })
 }
 
 export const down: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.removeIndex('prompts_chat_instances', 'prompts_chat_instances_chat_instance_id_prompt_id_idx')
+  await queryInterface.removeIndex('prompts_chat_instances', 'prompts_chat_instances_chat_instance_id_prompt_id_uniq')
 }
