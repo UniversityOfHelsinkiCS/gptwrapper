95d2ddbaa2c15adda2887e1f3644ca0214a96594
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-08-21 14:03:46 +0300

    add back course.activated logic
    misunderstood it and removed it while rebasing, now added it back

--- src/server/routes/ai/v3.ts
+++ src/server/routes/ai/v3.ts
@@ -55,8 +55,9 @@ router.post('/stream', upload.single('file'), async (r, res) => {
     }
 
     const isExempt = user.isAdmin || Boolean(course.responsibilities?.length)
-    if (!chatIsActive(course) && !isExempt) {
-      throw ApplicationError.Forbidden('Course is not active')
+    if (!isExempt) {
+      if (!course.activated) throw ApplicationError.Forbidden('Course chat is not activated')
+      if (!chatIsActive(course)) throw ApplicationError.Forbidden('Course is not active')
     }
 
     const [chatInstanceUsage] = await UserChatInstanceUsage.findOrCreate({
