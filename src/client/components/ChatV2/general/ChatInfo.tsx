1a0b794b304f7c298f48977cf08cbd2809a3545d
Jasse Merivirta <jasse.merivirta@helsinki.fi>
2026-09-08 11:45:16 +0300

    simplify ChatInfo further

--- src/client/components/ChatV2/general/ChatInfo.tsx
+++ src/client/components/ChatV2/general/ChatInfo.tsx
@@ -9,9 +9,6 @@ export const ChatInfo = ({ course }: { course: Course }) => {
 
   return (
     <Box mt={2} mb={4}>
-      <Typography variant="body1" fontStyle="italic">
-        {course.courseUnits.map((unit) => unit.code).join(', ')}
-      </Typography>
       <Typography variant="h5" sx={{ fontWeight: 'bold', mb: 1, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
         {course.name[language] || 'undefined course'}
       </Typography>
