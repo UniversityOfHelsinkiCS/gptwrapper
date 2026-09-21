7c284059c4aaadfec7910ef4117d56e6f339ac79
kaabero <katri.laamala@outlook.com>
2026-09-21 12:56:56 +0300

    add axe scan tests for prompt modal

--- e2e/checks/axe-scan.spec.ts
+++ e2e/checks/axe-scan.spec.ts
@@ -1,13 +1,21 @@
-import { test } from '@playwright/test'
-
+import { test, expect } from '@playwright/test'
 import { scan, writeAxeReport } from '../axe/axe_utils'
 
 test.describe('axe wcag scan', () => {
   test.afterAll(writeAxeReport)
 
-  test('scan home', async ({ page }, testInfo) => {
+  test('scan first load', async ({ page }, testInfo) => {
     await page.goto('/')
+    await expect(page.getByRole('heading', { level: 1 })).toBeVisible()
 
+    await scan(page, testInfo.title)
+  })
+
+  test('scan prompt modal', async ({ page }, testInfo) => {
+    await page.goto('/general')
+
+    await page.getByTestId('choose-prompt-button').click()
+    await expect(page.getByRole('heading', { level: 1 })).toBeVisible()
     await scan(page, testInfo.title)
   })
 })
