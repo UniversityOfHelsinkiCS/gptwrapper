0db03448bb005406971760be313fe56fab1f9c7d
kaabero <katri.laamala@outlook.com>
2026-09-23 14:36:54 +0300

    attempt to fix axe tests

--- e2e/checks/axe-scan.spec.ts
+++ e2e/checks/axe-scan.spec.ts
@@ -1,11 +1,14 @@
-import { test, expect } from '@playwright/test'
+import { expect } from '@playwright/test'
+import { teacherTest as test } from '../fixtures'
 import { scan, writeAxeReport } from '../axe/axe_utils'
+import { acceptDisclaimer } from '../utils/test-helpers'
 
 test.describe('axe wcag scan', () => {
   test.afterAll(writeAxeReport)
 
   test('scan first load', async ({ page }, testInfo) => {
     await page.goto('/')
+    await acceptDisclaimer(page)
     await expect(page.getByRole('heading', { level: 1 })).toBeVisible()
 
     await scan(page, testInfo.title)
@@ -13,17 +16,16 @@ test.describe('axe wcag scan', () => {
 
   test('scan prompt modal', async ({ page }, testInfo) => {
     await page.goto('/general')
-
+    await acceptDisclaimer(page)
     await page.getByTestId('choose-prompt-button').click()
     const modal = page.getByRole('dialog')
     await expect(modal).toBeVisible()
-    await expect(page.getByRole('heading', { level: 1 })).toBeVisible()
     await scan(page, testInfo.title)
   })
 
   test('scan global menu', async ({ page }, testInfo) => {
     await page.goto('/general')
-
+    await acceptDisclaimer(page)
     await page.getByTestId('global-menu-button').click()
     const modal = page.getByRole('menu')
     await expect(modal).toBeVisible()
